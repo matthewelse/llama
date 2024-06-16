41f8bca2cfb8716caadb75192fb980470b50f75b
Matthew Else <matthewelse1997@gmail.com>
2024-06-16 22:24:30 +0100

    Error handling

--- driver/llama_driver.ml
+++ driver/llama_driver.ml
@@ -10,6 +10,7 @@ let command =
       fun () ->
         In_channel.with_file (File_path.to_string source_file) ~f:(fun in_channel ->
           let lexbuf = Lexing.from_channel in_channel in
+          Lexing.set_filename lexbuf (File_path.to_string source_file);
           match Llama_frontend.Parser.program Llama_frontend.Lexer.read lexbuf with
           | exception Llama_frontend.Parser.Error n ->
             In_channel.seek in_channel 0L;
@@ -17,12 +18,13 @@ let command =
             let error_output =
               Diagnostics.create
                 ~code
+                ~message:"Syntax error"
                 ~error_code:[%string "E%{n#Int}"]
                 ~error_offset:(Lexing.lexeme_start_p lexbuf)
                 ~labels:
                   { primary =
                       { span = Lexing.lexeme_start_p lexbuf, Lexing.lexeme_end_p lexbuf
-                      ; message = "Syntax error"
+                      ; message = Llama_frontend.Errors.message n |> String.rstrip
                       }
                   ; secondary = []
                   }
--- frontend/llama_frontend.ml
+++ frontend/llama_frontend.ml
@@ -1,4 +1,5 @@
 module Ast = Ast
+module Errors = Errors
 module Expression = Expression
 module Lexer = Lexer
 module Parser = Parser
