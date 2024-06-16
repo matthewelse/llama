577bee9eb2871fe00fadd0116b3c35dc33b0690b
Matthew Else <matthewelse1997@gmail.com>
2024-06-16 21:21:34 +0100

    error reporting in the command line tool

--- driver/llama_driver.ml
+++ driver/llama_driver.ml
@@ -8,15 +8,32 @@ let command =
       let source_file = anon ("SRC" %: File_path.arg_type)
       and dump_ast = flag "dump-ast" no_arg ~doc:"Dump the AST after parsing" in
       fun () ->
-        let _ast =
-          In_channel.with_file (File_path.to_string source_file) ~f:(fun in_channel ->
-            let lexbuf = Lexing.from_channel in_channel in
-            let ast = Llama_frontend.Parser.program Llama_frontend.Lexer.read lexbuf in
+        In_channel.with_file (File_path.to_string source_file) ~f:(fun in_channel ->
+          let lexbuf = Lexing.from_channel in_channel in
+          match Llama_frontend.Parser.program Llama_frontend.Lexer.read lexbuf with
+          | exception Llama_frontend.Parser.Error n ->
+            In_channel.seek in_channel 0L;
+            let code = In_channel.input_all in_channel in
+            let error_output =
+              Diagnostics.create
+                ~code
+                ~error_code:[%string "E%{n#Int}"]
+                ~error_offset:(Lexing.lexeme_start_p lexbuf)
+                ~labels:
+                  { primary =
+                      { span = Lexing.lexeme_start_p lexbuf, Lexing.lexeme_end_p lexbuf
+                      ; message = "Syntax error"
+                      }
+                  ; secondary = []
+                  }
+                Error
+              |> Diagnostics.render
+            in
+            eprintf "%s\n" error_output
+          | ast ->
             if dump_ast
             then (
               Llama_utils.Pretty_print.pp_ast Format.std_formatter ast;
-              Format.pp_print_newline Format.std_formatter ());
-            ast)
-        in
+              Format.pp_print_newline Format.std_formatter ()));
         Deferred.Or_error.return ()]
 ;;
